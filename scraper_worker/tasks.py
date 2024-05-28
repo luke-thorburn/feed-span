0c510995541f2d914d1f6aa2524fdffdf87f209e
Luke Thorburn <luke.thorburn@gmail.com>
2024-05-28 07:21:11 -0400

    Updated sample code

--- scraper_worker/tasks.py
+++ scraper_worker/tasks.py
@@ -6,6 +6,10 @@ from typing import Optional
 
 import requests
 from twscrape import API, gather
+from util.scheduler import ScheduledTask, schedule_tasks
+
+from scraper_worker.celery_app import app
+from scraper_worker.ingester import ErrorData, IngestData, SuccessData
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,9 +17,6 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
-
-from scraper_worker.celery_app import app
-from scraper_worker.ingester import ErrorData, IngestData, SuccessData
 
 
 def send_result(task_id: str, results: list[dict], error: Optional[str] = None):
@@ -102,7 +103,13 @@ def setup_periodic_tasks(sender, **kwargs):
             "args": ["formula one", 10],
         },
     }
-    for task_id, task in task_manifest.items():
-        sender.add_periodic_task(
-            600, task["function"].s(*task["args"]).set(task_id=task_id), name=task_id
+    scheduled_tasks = [
+        ScheduledTask(
+            task["function"],
+            args=task["args"],
+            options={"task_id": task_id},
+            interval_seconds=600,
         )
+        for task_id, task in task_manifest.items()
+    ]
+    schedule_tasks(app, scheduled_tasks, logger=logger)
