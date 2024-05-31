58264604a6d2ffb47eec07bbf01e59a5dedf5cc3
Luke Thorburn <luke.thorburn@gmail.com>
2024-05-31 12:03:22 -0400

    Implementing scraper

--- examples/combined/scraper_worker/tasks.py
+++ examples/combined/scraper_worker/tasks.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from typing import Optional
 
 import requests
-from twscrape import API, gather
 from util.scheduler import ScheduledTask, schedule_tasks
 
 from scraper_worker.celery_app import app
@@ -53,38 +52,26 @@ def process_error(task_id: str, message: str):
     return send_result(task_id, [], message)
 
 
-async def _twitter_search_top(query: str, limit: int = 10) -> list[dict]:
-    cookies = os.getenv("TWITTER_SESSION_COOKIE")
-    username = os.getenv("TWITTER_USERNAME")
-    email = os.getenv("TWITTER_EMAIL")
-    if not all([cookies, username, email]):
-        raise ValueError("Twitter session cookie, username, and email must be set.")
-
-    api = API()
-    await api.pool.add_account(
-        username, "dummy-pass", email, "dummy-mail-pass", cookies=cookies
-    )  # type: ignore
-
-    tweets = await gather(api.search(query, limit=limit, kv={"product": "Top"}))
+async def _apify_query(query: str, limit: int = 10) -> list[dict]:
+    
+    # TODO - Add API calls here.
     results = []
-    for tweet in tweets:
-        results.append(tweet.dict())
+    
     return results
 
 
 @app.task(bind=True)
-def twitter_search_top(self, query: str, limit: int = 10) -> None:
-    """Use the Twitter session to search for tweets and return results from
-    the "top" section.
+def apify_query(self, platform: str, limit: int = 10) -> None:
+    """Regular query of Apify APIs.
 
     Args:
-        query (str): search query to use.
+        platform (str): which platform to scrape.
         limit (int): number of results to return.
     """
 
     task_id = self.request.id
     try:
-        results = asyncio.run(_twitter_search_top(query, limit))
+        results = asyncio.run(_apify_query(platform, limit))
         process_success(task_id, results)
     except Exception as e:
         process_error(task_id, str(e))
@@ -94,13 +81,17 @@ def twitter_search_top(self, query: str, limit: int = 10) -> None:
 def setup_periodic_tasks(sender, **kwargs):
     """Setup periodic tasks for the worker."""
     task_manifest = {
-        "twitter_top_musk": {
-            "function": twitter_search_top,
-            "args": ["elon musk", 10],
+        "twitter": {
+            "function": apify_query,
+            "args": ["twitter"],
         },
-        "twitter_top_f1": {
-            "function": twitter_search_top,
-            "args": ["formula one", 10],
+        "facebook": {
+            "function": apify_query,
+            "args": ["facebook"],
+        },
+        "reddit": {
+            "function": apify_query,
+            "args": ["reddit"],
         },
     }
     scheduled_tasks = [
