07ca8f5c6d860c63f58fa5594fbb3bab1321fb65
Soham De <soham.de2001@gmail.com>
2024-06-18 06:35:47 -0700

    updated classifiers in scorer workers

---
+++ components/scorer_worker/classifiers.py
@@ -0,0 +1,93 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import DistilBertModel, DistilBertTokenizer, DistilBertForSequenceClassification
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+#print("Current working directory:", os.getcwd())
+
+
+# Check if GPU is available and move the model to the appropriate device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+civic_tokenizer = AutoTokenizer.from_pretrained("scorer_worker/model_civic")
+civic_model = AutoModelForSequenceClassification.from_pretrained("scorer_worker/model_civic")
+civic_model.to(device)
+
+# Load pre-trained BERT model and tokenizer
+bridge_tokenizer = DistilBertTokenizer.from_pretrained("scorer_worker/model_bridging")
+bridge_model = DistilBertForSequenceClassification.from_pretrained("scorer_worker/model_bridging") 
+bridge_model.to(device)
+
+def areCivic(texts):
+
+
+    # Tokenize the list of texts
+    inputs = civic_tokenizer(texts, return_tensors="pt", truncation=True, padding=True).to(device)
+
+    # Perform the classification
+    with torch.no_grad():
+        outputs = civic_model(**inputs)
+        logits = outputs.logits
+
+    # Convert logits to probabilities
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+
+    # Get the predicted labels
+    predicted_labels = torch.argmax(probs, dim=1).tolist()
+    print(predicted_labels)
+
+    # Return list of booleans indicating if each text is "civic"
+    return [label == 1 for label in predicted_labels]
+
+def getBridgeScore(text):
+
+    # Tokenize the input text
+    logger.info(f'===== running on {device}')
+    inputs = bridge_tokenizer(text, return_tensors='pt', padding=True, truncation=True).to(device)
+    logger.info(f'===== tokenized')
+    input_ids = inputs['input_ids'].to(device)
+    attention_mask = inputs['attention_mask'].to(device)
+    
+    # Perform inference
+    bridge_model.eval()
+    with torch.no_grad():
+        outputs = bridge_model(input_ids, attention_mask=attention_mask)
+        prediction = outputs.logits.item()
+    logger.info(f'===== predicted !!!')
+    
+    return prediction
+
+
+def isCivic(text):
+
+    inputs = civic_tokenizer(text, return_tensors="pt", truncation=True, padding=True).to(device)
+    logger.info(f'===== tokenized')
+    # Perform the classification
+    civic_model.eval()
+    with torch.no_grad():
+        outputs = civic_model(**inputs)
+        logits = outputs.logits
+    
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+
+    predicted_label = torch.argmax(probs, dim=1).item()
+
+    if predicted_label == 1:
+        return True
+    else:
+        return False
+
+  
+
+if __name__ == "__main__":
+    print("Hello, World!")
+
+
--- components/scorer_worker/tasks.py
+++ components/scorer_worker/tasks.py
@@ -34,85 +34,143 @@ Models:
     SentimentScoreOutput
 """
 
-# import logging
-# import random
-# import time
-# from typing import Any
+import logging
+import random
+import time
+from typing import Any
 
-# from pydantic import BaseModel, Field
-# from ranking_server.classifiers import areCivic, getBridgeScore
-
-
-# from scorer_worker.celery_app import app
-
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s [%(levelname)s] %(message)s",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-# )
-# logger = logging.getLogger(__name__)
+from pydantic import BaseModel, Field
+from scorer_worker.classifiers import isCivic, areCivic, getBridgeScore
 
 
-# KILL_DEADLINE_SECONDS = 10
-# TIME_LIMIT_SECONDS = 10
+from scorer_worker.celery_app import app
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+KILL_DEADLINE_SECONDS = 10
+TIME_LIMIT_SECONDS = 5
 
 
-# class CivicLabelInput(BaseModel):
-#     item_id: str = Field(description="The ID of the item to label")
-#     text: str = Field(description="The body of the post for label")
+class CivicLabelInput(BaseModel):
+    item_id: str = Field(description="The ID of the item to label")
+    text: str = Field(description="The body of the post for label")
 
-# class BridgeScoreInput(BaseModel):
-#     item_id: str = Field(description="The ID of the item to score")
-#     text: str = Field(description="The body of the post for scoring")
+#class BridgeScoreInput(BaseModel):
+#    item_id: str = Field(description="The ID of the item to score")
+#    text: str = Field(description="The body of the post for scoring")
 
-# class ScoreOutput(BaseModel):
-#     item_id: str = Field(description="The ID of the item to score")
-#     score: float = Field(description="Numerical score")
-#     t_start: float = Field(description="Start time (seconds)", default=0)
-#     t_end: float = Field(description="End time (seconds)", default=0)
+class ScoreOutput(BaseModel):
+    item_id: str = Field(description="The ID of the item to score")
+    score: float = Field(description="Numerical score")
+    t_start: float = Field(description="Start time (seconds)", default=0)
+    t_end: float = Field(description="End time (seconds)", default=0)
 
+class LabelOutput(BaseModel):
+    item_id: str = Field(description="The ID of the item to score")
+    label: bool = Field(description="Boolean label")
+    t_start: float = Field(description="Start time (seconds)", default=0)
+    t_end: float = Field(description="End time (seconds)", default=0)
 
-# class BridgeScoreOutput(ScoreOutput):
-#     pass
+class RandomScoreInput(BaseModel):
+    item_id: str = Field(description="The ID of the item to score")
+    text: str = Field(description="The body of the post for scoring")
+    mean: float = Field(description="Mean of the random score", default=0.5)
+    sdev: float = Field(description="Standard deviation of the radom score", default=0.1)
+    sleep: float | None = Field(description="Sleep time for testing", default=None)
+    raise_exception: bool = Field(description="Raise an exception for testing", default=False)
 
-# class CivicLabelOutput(ScoreOutput):
-#     pass
+class BridgeScoreOutput(ScoreOutput):
+    pass
+
+class CivicLabelOutput(LabelOutput):
+    pass
+
+class RandomScoreOutput(ScoreOutput):
+    pass
+
 
 # class TimeoutException(Exception):
 #     pass
 
 
-# def do_civic_labelling(input: CivicLabelInput) -> CivicLabelOutput:
-#     label = areCivic(input.text)
-#     return CivicLabelOutput(
-#         item_id=input.item_id,
-#         score=label[0],
-#     )
+def do_civic_labelling(input: CivicLabelInput) -> CivicLabelOutput:
+    label = isCivic(input.text)
+    return CivicLabelOutput(
+        item_id=input.item_id,
+        label=label,
+    )
 
 
-# @app.task(bind=True, time_limit=KILL_DEADLINE_SECONDS, soft_time_limit=TIME_LIMIT_SECONDS)
-# def civic_labeller(self, **kwargs) -> dict[str, Any]:
-#     """ Model to classify civic content
+@app.task(bind=True, time_limit=KILL_DEADLINE_SECONDS, soft_time_limit=TIME_LIMIT_SECONDS)
+def civic_labeller(self, **kwargs) -> dict[str, Any]:
+    """ Model to classify civic content
 
-#     Args:
-#         **kwargs: Arbitrary keyword arguments. These should be convertible to CivicLabelInput,
-#                   thus the input should contain `item_id` and `text`
+    Args:
+        **kwargs: Arbitrary keyword arguments. These should be convertible to CivicLabelInput,
+                  thus the input should contain `item_id` and `text`
 
-#     Returns:
-#         dict[str, Any]: The result of the sentiment scoring task. The result is a dictionary
-#                         representation of CivicLabelOutput
+    Returns:
+        dict[str, Any]: The result of the sentiment scoring task. The result is a dictionary
+                        representation of CivicLabelOutput
 
-#     The results are stored in the Celery result backend.
-#     """
-#     start = time.time()
-#     task_id = self.request.id
-#     worker_id = self.request.hostname
-#     logger.info(f"Task {task_id} started by {worker_id}")
-#     input = CivicLabelInput(**kwargs)
-#     result = do_civic_labelling(input)
-#     result.t_start = start
-#     result.t_end = time.time()
-#     return result.model_dump()
+    The results are stored in the Celery result backend.
+    """
+    start = time.time()
+    task_id = self.request.id
+    worker_id = self.request.hostname
+    logger.info(f"Task {task_id} started by {worker_id}")
+
+    input = CivicLabelInput(**kwargs)
+    result = do_civic_labelling(input)
+    result.t_start = start
+    result.t_end = time.time()
+    print(f'time to score: {result.t_end - result.t_start}')
+    return result.model_dump()
+
+
+
+def do_random_scoring(input: RandomScoreInput) -> RandomScoreOutput:
+
+    if input.sleep:
+        time.sleep(input.sleep)
+    if input.raise_exception:
+        raise ValueError("Random exception")
+    return RandomScoreOutput(
+        item_id=input.item_id,
+        score=random.normalvariate(input.mean, input.sdev),
+    )
+
+
+@app.task(bind=True, time_limit=KILL_DEADLINE_SECONDS, soft_time_limit=TIME_LIMIT_SECONDS)
+def random_scorer(self, **kwargs) -> dict[str, Any]:
+    """Output random score
+
+    Args:
+        **kwargs: Arbitrary keyword arguments. These should be convertible to RandomScoreInput.
+                  Fields `sleep` and `raise_exception` can be used for load and failure testing.
+
+    Returns:
+        dict[str, Any]: The result of the sentiment scoring task. The result is a dictionary
+                        representation of RandomScoreOutput
+
+    The results are stored in the Celery result backend.
+    """
+
+    logger.info('in the random scorer =====')
+    start = time.time()
+    task_id = self.request.id
+    worker_id = self.request.hostname
+    logger.info(f"Task {task_id} started by {worker_id}")
+    input = RandomScoreInput(**kwargs)
+    result = do_random_scoring(input)
+    result.t_start = start
+    result.t_end = time.time()
+    return result.model_dump()
 
 
 # def do_bridge_scoring(input: BridgeScoreInput) -> BridgeScoreOutput:
