d305a2d20bfa6054a231e88221e2d40d608b3f06
Luke Thorburn <luke.thorburn@gmail.com>
2024-06-19 20:07:09 -0400

    Bug fixes

--- components/sandbox_worker/tasks.py
+++ components/sandbox_worker/tasks.py
@@ -6,10 +6,6 @@ import psycopg2
 import redis
 from util.scheduler import ScheduledTask, schedule_tasks
 
-if __name__ == "__main__":
-    import torch.multiprocessing as mp
-    mp.set_start_method('spawn')
-
 from sandbox_worker.classifiers import isCivic, getBridgeScore
 
 from sandbox_worker.celery_app import app
